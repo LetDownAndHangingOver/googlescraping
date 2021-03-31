3482de50d204aefa4ed8b4d47e0672760fec84af
Benali <benalimoe@gmail.com>
2021-03-31 21:33:43 +0200

    adding mongodb

--- ESTIAMProject/googlescrape/webscrapingestiam/pipelines.py
+++ ESTIAMProject/googlescrape/webscrapingestiam/pipelines.py
@@ -6,8 +6,24 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+import pymongo
+import logging
+
+#
+class MongodbPipeline:
+
+    collection_name = "google articles"
+
+    def open_spider(self, spider):
+        logging.warning("test 1 ")
+        self.client = pymongo.MongoClient("mongodb+srv://moe:testtest@cluster0.pmexa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+        self.db = self.client["google"]
+        logging.warning("test 2 ")
+
+    def close_spider(self, spider):
+        self.client.close()
 
 
-class WebscrapingestiamPipeline:
     def process_item(self, item, spider):
+        self.db[self.collection_name].insert(item)
         return item
--- ESTIAMProject/googlescrape/webscrapingestiam/spiders/google.py
+++ ESTIAMProject/googlescrape/webscrapingestiam/spiders/google.py
@@ -1,5 +1,6 @@
 import scrapy
 from newspaper import Article
+from newspaper import Config
 import random
 import time
 
@@ -32,10 +33,12 @@ class GoogleSpider(scrapy.Spider):
             'keywords' : KW,
             'keywordIndex' : keywordIndex,
             'page': page
-        })
+        }, dont_filter=True)
 
     def parse(self, response):
+        print('ch7al?')
         userAgents = response.request.meta['userAgents']
+        userA = userAgents[random.randint(1, 77)]
         KW = response.request.meta['keywords']
         keywordIndex = response.request.meta['keywordIndex']
         page = response.request.meta['page']
@@ -43,7 +46,8 @@ class GoogleSpider(scrapy.Spider):
         liens = response.xpath("//div[@class='hlcw0c']/div/div/div/a")
         #print(liens)
         for lien in liens: #~10
-            titre = lien.xpath(".//text()").get()
+            print('lien')
+            titre = lien.xpath(".//h3/text()").get()
             lien2 = lien.xpath(".//@href").get()
             title, lienn, author, summary, keywords = self.newss(lien2)
             yield{
@@ -56,38 +60,41 @@ class GoogleSpider(scrapy.Spider):
             
         
         page = page + 10
-        userA = userAgents[random.randint(1, 77)]
         print(f'keywooord {keywordIndex}')
         if keywordIndex < 9:
-            if page < 100:
+            if page < 30:
                 print("oui ou pas?")
                 scrapingURL = f'https://google.com/search?q={KW[keywordIndex]}&start={page}'
+                print(scrapingURL)
                 yield scrapy.Request(url=scrapingURL, callback=self.parse, headers={
                     'User-Agent': userA
                 }, meta={
-                    'userA' : userAgents,
+                    'userAgents' : userAgents,
                     'keywords' : KW,
                     'keywordIndex' : keywordIndex,
                     'page': page
-                })
+                }, dont_filter=True)
                 #time.sleep(60)
             if page == 20:
                 print("on quitte la")
                 keywordIndex = keywordIndex + 1
+                page = 0
                 scrapingURL = f'https://google.com/search?q={KW[keywordIndex]}&start={page}'
                 yield scrapy.Request(url=scrapingURL, callback=self.parse, headers={
                     'User-Agent': userA
                 }, meta={
-                    'userA' : userAgents,
+                    'userAgents' : userAgents,
                     'keywords' : KW,
                     'keywordIndex' : keywordIndex,
                     'page': page
-                })
+                }, dont_filter=True)
                 #time.sleep(60)
         
     def newss(self, lienn):
         print(f"le lien est : {lienn}")
-        article = Article(lienn, language="fr")
+        config = Config()
+        config.browser_user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
+        article = Article(lienn, language="fr", config=config)
         article.download()
         article.parse()
         title = article.title
